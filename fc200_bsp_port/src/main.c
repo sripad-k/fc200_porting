23fedae43581b5b8ff7f1cd89ded954307bc992e
sripad-k <sripad.kulkarni@lodd.com>
2025-10-30 10:54:28 +0400

    Round-1: UART write working

--- fc200_bsp_port/src/main.c
+++ fc200_bsp_port/src/main.c
@@ -1,11 +1,19 @@
-#include "soc/defines/d_common_types.h"
-#include "soc/defines/d_common_status.h"
-#include "kernel/general/d_gen_string.h"
+#include "type.h"
 #include "bsp_srv/interface/uart_interface.h"
+#include "soc/timer/d_timer.h"
 
 Int32_t main()
 {
 
+    d_TIMER_Initialise();
     uart_init(UART_DEBUG_CONSOLE);
+    const Char_t hello_message[80] = "\n\rHello, this is the FC-200.\n\r";
+
+    while(1)
+    {
+        // Main loop
+        d_TIMER_DelayMilliseconds(100);
+        uart_write(UART_DEBUG_CONSOLE, (uint8_t*)hello_message, sizeof(hello_message));
+    }
 
 }
--- fc200_bsp_port/src/type.h
+++ fc200_bsp_port/src/type.h
@@ -1,7 +1,8 @@
 // #include "stdint.h"
 //#include "stdbool.h"
 #include "soc/defines/d_common_types.h"
-
+#include "soc/defines/d_common_status.h"
+#include "kernel/general/d_gen_string.h"
 
 typedef Int8_t      int8_t;
 typedef Uint8_t     uint8_t;
@@ -15,3 +16,23 @@ typedef Uint64_t    uint64_t;
 typedef Bool_t bool;           // typedef the type
 #define false d_FALSE          // alias the literal
 #define true d_TRUE            // alias the literal
+
+// 8-bit types
+#define INT8_MAX    127
+#define INT8_MIN    (-128)
+#define UINT8_MAX   255
+
+// 16-bit types
+#define INT16_MAX   32767
+#define INT16_MIN   (-32768)
+#define UINT16_MAX  65535
+
+// 32-bit types
+#define INT32_MAX   2147483647
+#define INT32_MIN   (-2147483648)
+#define UINT32_MAX  4294967295U
+
+// 64-bit types
+#define INT64_MAX   9223372036854775807LL
+#define INT64_MIN   (-9223372036854775807LL - 1)
+#define UINT64_MAX  18446744073709551615ULL
